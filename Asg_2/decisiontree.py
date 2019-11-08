27c0af546052851522d223863964387d63bcb5e9
Smrutisudhadash <sudha.1922@gmail.com>
2019-11-08 13:00:52 -0700

    Update decisiontree.py

--- Asg_2/decisiontree.py
+++ Asg_2/decisiontree.py
@@ -8,16 +8,21 @@ import pandas as pd
 import numpy as np
 import pickle
 import PCAonFeatureMatrix as pcafeature
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+#from sklearn.model_selection import cross_val_score
 
 def decisiontest(data):
     train=data.iloc[:,0:5]
     label = data.iloc[:,5]
-    dt = DecisionTreeClassifier()
-    kf = KFold(n_splits=5)
-    i=1
+    dt = DecisionTreeClassifier(random_state = 0)
+    kf = KFold(n_splits=6, shuffle = True)
     ascore=[]
+    pr=[]
+    rc=[]
+    f1=[]
     for train_index, test_index in kf.split(train, label):
         train_data, train_l = train.iloc[train_index], label.iloc[train_index]
         test_data, test_l = train.iloc[test_index], label.iloc[test_index]
@@ -25,9 +30,16 @@ def decisiontest(data):
         predictions = dt.predict(test_data)
         a = accuracy_score(predictions,test_l)
         ascore.append(a)
-        print("Accuracy ",i," :", a)
-        i+=1
+        pr.append(precision_score(predictions, test_l))
+        rc.append(recall_score(predictions, test_l))
+        f1.append(f1_score(predictions, test_l))
+    #print("k=",j)
+    print("Accuracy:", ascore)
     print("Mean Accuracy: ", sum(ascore)/len(ascore))
+    print("Precision: ",sum(pr)/len(pr))
+    print("Recall:", sum(rc)/len(rc))
+    print("F1 Score: ",sum(f1)/len(f1))
+    #print("---------------------------------------------------------------------------------------------")
     pickle.dump(dt,open('dt_model.sav','wb'))
 
     # print("Printing score:---------------------------------------------------------------------------------------")
@@ -36,22 +48,20 @@ def decisiontest(data):
     #     s= cross_val_score(dt, train, label, cv=i, scoring='accuracy')
     #     print("Score=",s.mean())
     #     print("-----------------------------------------------------------------------------------------------------")
-    
-
-    #print("Plotting tree: ---------------------------------------------------------------------")
-    #tree.plot_tree(dt)
-    # dot_data = StringIO()
-    # export_graphviz(dt, out_file=dot_data, filled=True, rounded=True, special_characters=True)
-    # graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-    # Image(graph.create_png())
 
 def decisiontrain(test_data,test_label):
     dt = pickle.load(open('dt_model.sav', 'rb'))
     predictions = dt.predict(test_data)
-    print(accuracy_score(test_label,predictions))
+    print("Decision Tree Accuracy: ", accuracy_score(test_label,predictions))
+    print("Decision Tree precision = ",precision_score(test_label,predictions))
+    print("Decision Tree recall = ",recall_score(test_label,predictions))
+    print("Decision Tree F1 = ",f1_score(test_label,predictions))
+    print("Decision Tree Confusion Matrix :")
+    print(confusion_matrix(predictions, test_label))
+
 
 def dt_test_one(test_data):
-    df = pickle.load(open('dt_model.sav', 'rb'))
+    dt = pickle.load(open('dt_model.sav', 'rb'))
     predictions = dt.predict(test_data)
     print("Predicted class:", predictions)
     return predictions
