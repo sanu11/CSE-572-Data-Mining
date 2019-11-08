38e1f6c8df48e9ba7ca0d195cc41741699b1cfd0
Smrutisudhadash <sudha.1922@gmail.com>
2019-11-08 10:27:48 -0700

    Update decisiontree.py

--- Asg_2/decisiontree.py
+++ Asg_2/decisiontree.py
@@ -8,31 +8,58 @@ import pandas as pd
 import numpy as np
 import pickle
 import PCAonFeatureMatrix as pcafeature
-from sklearn.externals.six import StringIO  
-from IPython.display import Image  
-from sklearn.tree import export_graphviz
-import pydotplus
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
-def decisiontest(train_data, train_label):
+def decisiontest(data):
+    train=data.iloc[:,0:5]
+    label = data.iloc[:,5]
     dt = DecisionTreeClassifier()
-    dt.fit(train_data, train_label)
-    #print("Tre:-----------------------------------------------------------------------------------")
-    #print(dt)
+    kf = KFold(n_splits=5)
+    i=1
+    ascore=[]
+    for train_index, test_index in kf.split(train, label):
+        train_data, train_l = train.iloc[train_index], label.iloc[train_index]
+        test_data, test_l = train.iloc[test_index], label.iloc[test_index]
+        dt.fit(train_data, train_l)
+        predictions = dt.predict(test_data)
+        a = accuracy_score(predictions,test_l)
+        ascore.append(a)
+        print("Accuracy ",i," :", a)
+        i+=1
+    print("Mean Accuracy: ", sum(ascore)/len(ascore))
+    pickle.dump(dt,open('dt_model.sav','wb'))
+
+    # print("Printing score:---------------------------------------------------------------------------------------")
+    # for i in range(2,11):
+    #     print("i = ",i)
+    #     s= cross_val_score(dt, train, label, cv=i, scoring='accuracy')
+    #     print("Score=",s.mean())
+    #     print("-----------------------------------------------------------------------------------------------------")
+    
+
     #print("Plotting tree: ---------------------------------------------------------------------")
     #tree.plot_tree(dt)
     # dot_data = StringIO()
     # export_graphviz(dt, out_file=dot_data, filled=True, rounded=True, special_characters=True)
     # graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
     # Image(graph.create_png())
-    pickle.dump(dt,open('dt_model.sav','wb'))
 
 def decisiontrain(test_data,test_label):
     dt = pickle.load(open('dt_model.sav', 'rb'))
     predictions = dt.predict(test_data)
     print(accuracy_score(test_label,predictions))
 
-train_X,train_Y,test_X, test_Y = pcafeature.get_feature_matrix_Final('Asg_2/Data/concatenatedData.csv')
-decisiontest(train_X, train_Y)
-decisiontrain(test_X,test_Y)
+def dt_test_one(test_data):
+    df = pickle.load(open('dt_model.sav', 'rb'))
+    predictions = dt.predict(test_data)
+    print("Predicted class:", predictions)
+    return predictions
+
+#train_X,train_Y,test_X, test_Y = 
+#data = pd.read_csv("final_matrix_for_models.csv") 
+data = pcafeature.get_feature_matrix_Final('Asg_2/Data/concatenatedData.csv')
+decisiontest(data)
+#decisiontrain(test_X,test_Y)
 
 
